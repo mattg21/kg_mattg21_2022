529087d9cd97f25dd6746f97a0ad69d0714835e9
MATTHEW GAGNON <mgnon@bu.edu>
2021-04-26 18:14:39 -0400

    Completed

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
-let numberToPhonetic = {1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven", 8: "eight",9: "nine",0: "zero"};
+let number_to_phonetic = {'1': "One", '2': "Two", '3': "Three", '4': "Four", '5': "Five", '6': "Six", '7': "Seven", '8': "Eight",'9': "Nine",'0': "Zero"};
 
 for(i=0; i <= 9; i++) {
-    console.log(numberToPhonetic[i])
+    console.log(number_to_phonetic[i.toString()])
 }
-let myArgs = process.argv.slice(2)
-console.log(myArgs)
+let my_args = process.argv.slice(2)
+console.log(my_args)
+
+let my_converted = "";
+
+for(i = 0; i < my_args.length; i++) {
+    try {
+        let int_to_convert = my_args[i].split('')
+        //console.log(int_to_convert)
+        let curr_string = "";
+        for (j = 0; j < int_to_convert.length; j++) {
+            curr_string += number_to_phonetic[int_to_convert[j]]    
+        }
+        if (i == my_args.length - 1) {
+            my_converted += curr_string 
+        }else {
+            my_converted += curr_string + ","
+        }
+    }catch(err) {
+
+    }
+    
+}
+console.log(my_converted)
