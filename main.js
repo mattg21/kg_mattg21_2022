584cfa10ad672a1d9b9021faf28c917a85f5f379
MATTHEW GAGNON <mgnon@bu.edu>
2021-04-26 19:35:10 -0400

    function implementation

--- main.js
+++ main.js
@@ -1,29 +1,28 @@
-let number_to_phonetic = {'1': "One", '2': "Two", '3': "Three", '4': "Four", '5': "Five", '6': "Six", '7': "Seven", '8': "Eight",'9': "Nine",'0': "Zero"};
+const my_args = process.argv.slice(2)
 
-for(i=0; i <= 9; i++) {
-    console.log(number_to_phonetic[i.toString()])
-}
-let my_args = process.argv.slice(2)
-console.log(my_args)
-
-let my_converted = "";
-
-for(i = 0; i < my_args.length; i++) {
+function int_to_string(m_args)  {
+    const number_to_phonetic = {'1': "One", '2': "Two", '3': "Three", '4': "Four", '5': "Five", '6': "Six", '7': "Seven", '8': "Eight",'9': "Nine",'0': "Zero"};
     try {
-        let int_to_convert = my_args[i].split('')
-        //console.log(int_to_convert)
-        let curr_string = "";
-        for (j = 0; j < int_to_convert.length; j++) {
-            curr_string += number_to_phonetic[int_to_convert[j]]    
-        }
-        if (i == my_args.length - 1) {
-            my_converted += curr_string 
-        }else {
-            my_converted += curr_string + ","
-        }
-    }catch(err) {
+        let my_converted = ""
 
+        for(i = 0; i < m_args.length; i++) {
+
+            let int_to_convert = m_args[i].split('')
+            let curr_string = "";
+
+            for (j = 0; j < int_to_convert.length; j++) {
+                curr_string += number_to_phonetic[int_to_convert[j]]    
+            }
+            
+            if (i == m_args.length - 1) {
+                my_converted += curr_string 
+            }else {
+                my_converted += curr_string + ","
+            }
+            
+        } return my_converted 
     }
-    
 }
-console.log(my_converted)
+console.log(my_args)
+const myString = int_to_string(my_args)
+console.log(myString)
