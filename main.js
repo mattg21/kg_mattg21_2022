c2b07fd9852e22fcca45123979c7b716bf964872
MATTHEW GAGNON <mgnon@bu.edu>
2021-04-26 20:30:25 -0400

    Fixed args

--- main.js
+++ main.js
@@ -1,33 +1,38 @@
 const my_args = process.argv.slice(2)
 
+function int_ch_array_to_phonetic(int_ch_array) {
+    const number_to_phonetic = {'1': "One", '2': "Two", '3': "Three", '4': "Four", '5': "Five", '6': "Six", '7': "Seven", '8': "Eight",'9': "Nine",'0': "Zero"}
+    let curr_string = ""
+    
+    for (j = 0; j < int_ch_array.length; j++) {
+        curr_string += number_to_phonetic[int_ch_array[j]]    
+    }
+    return curr_string
+}
 
 function int_to_string(m_args)  {
-    const number_to_phonetic = {'1': "One", '2': "Two", '3': "Three", '4': "Four", '5': "Five", '6': "Six", '7': "Seven", '8': "Eight",'9': "Nine",'0': "Zero"};
+    // takes in a list of args passed in as integers
     try {
         let my_converted = ""
 
         for(i = 0; i < m_args.length; i++) {
-
-            let int_to_convert = m_args[i].split('')
-            let curr_string = "";
-
-            for (j = 0; j < int_to_convert.length; j++) {
-                curr_string += number_to_phonetic[int_to_convert[j]]    
+            if(isNaN(m_args[i])) {
+                console.log("Invalid argument")
+                return ""
             }
-
-            if (i == m_args.length - 1) {
-                my_converted += curr_string 
-            }else {
-                my_converted += curr_string + ","
+            const int_to_convert = m_args[i].split('')
+            if(i == m_args.length - 1 ) {
+                my_converted += int_ch_array_to_phonetic(int_to_convert)
+            } else {
+                my_converted += int_ch_array_to_phonetic(int_to_convert) + ','
             }
             
         } return my_converted 
         
     }catch(err) {
-        console.log("Invalid argument")
+        console.log("Something went wrong: ", err)
     }
 }
 
-
 const myString = int_to_string(my_args)
-console.log(myString)
+if (myString) console.log(myString); 
